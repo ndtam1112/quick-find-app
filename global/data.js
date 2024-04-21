d69b866fb90b67415da2f81021d75951a11d50f0
ndtam1112 <ndtam1010@gmail.com>
2024-04-22 04:16:45 +0700

    .

---
+++ global/data.js
@@ -0,0 +1,31 @@
+export const centerAround = [
+  {
+    nameCenter: 'Cấp',
+    numberCenter: '',
+    location: 'Văn',
+    latitude: 20,
+    longitude: 105,
+  },
+  {
+    nameCenter: 'Cấp cứu 911 Việt Nam',
+    numberCenter: '',
+    location: 'Văn Khê, Khu đô thị Văn Khê, Hà Đông, Hà Nội, Việt Nam',
+    latitude: 20.977031041316064,
+    longitude: 105.76277515152955,
+  },
+  {
+    nameCenter: 'Trung tâm Cấp cứu 911 Việt Nam',
+    numberCenter: '0346911911',
+    location: '127 P. Đốc Ngữ, Liễu Giai, Ba Đình, Hà Nội, Việt Nam',
+    latitude: 21.0427331617877,
+    longitude: 105.81290026808536,
+  },
+  {
+    nameCenter: 'Đội xe cấp cứu 911 Việt Nam',
+    numberCenter: '0346911911',
+    location:
+      'Trung tâm Giáo dục Thường xuyên Ba Đình, TT 301, 301 P. Đội Cấn, Cống Vị, Ba Đình, Hà Nội 100000, Việt Nam',
+    latitude: 21.040490152094602,
+    longitude: 105.81393023623376,
+  },
+]
--- screens/Home/SelectTo.jsx
+++ screens/Home/SelectTo.jsx
@@ -1,6 +1,6 @@
 import { Entypo, FontAwesome5, MaterialIcons } from '@expo/vector-icons'
 import { Button, Flex, Text } from '@react-native-material/core'
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import {
   Image,
   Keyboard,
@@ -12,6 +12,144 @@ import {
   TouchableWithoutFeedback,
   View,
 } from 'react-native'
+import centerAround from '../../global/data.js'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import * as Location from 'expo-location'
+
+const SelectTo = ({ navigation }) => {
+  const [latlng, setLatLng] = useState({})
+  const _map = useRef(1)
+  const checkPermission = async () => {
+    const hasPermission = await Location.requestForegroundPermissionsAsync()
+    if (hasPermission.status === 'granted') {
+      const permission = await askPermission()
+      return permission
+    }
+    return true
+  }
+  const askPermission = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync()
+    return permission.status === 'granted'
+  }
+  const getLocation = async () => {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync()
+      if (!graned) return
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync()
+      setLatLng({ latitude: latitude, longitude: longitude })
+    } catch (err) {}
+  }
+  useEffect(() => {
+    checkPermission()
+    getLocation()
+    console.log(latlng), []
+  })
+
+  const pressSetFrom = () => {
+    navigation.navigate('SetFrom')
+  }
+  const pressHanderBack = () => {
+    navigation.goBack()
+  }
+
+  const centerAround = [
+    {
+      nameCenter: 'Cấp',
+      numberCenter: '',
+      location: 'Văn',
+      latitude: 20,
+      longitude: 105,
+    },
+    {
+      nameCenter: 'Cấp cứu 911 Việt Nam',
+      numberCenter: '',
+      location: 'Văn Khê, Khu đô thị Văn Khê, Hà Đông, Hà Nội, Việt Nam',
+      latitude: 20.977031041316064,
+      longitude: 105.76277515152955,
+    },
+    {
+      nameCenter: 'Trung tâm Cấp cứu 911 Việt Nam',
+      numberCenter: '0346911911',
+      location: '127 P. Đốc Ngữ, Liễu Giai, Ba Đình, Hà Nội, Việt Nam',
+      latitude: 21.0427331617877,
+      longitude: 105.81290026808536,
+    },
+    {
+      nameCenter: 'Đội xe cấp cứu 911 Việt Nam',
+      numberCenter: '0346911911',
+      location:
+        'Trung tâm Giáo dục Thường xuyên Ba Đình, TT 301, 301 P. Đội Cấn, Cống Vị, Ba Đình, Hà Nội 100000, Việt Nam',
+      latitude: 21.040490152094602,
+      longitude: 105.81393023623376,
+    },
+  ]
+
+  centerAround.map(centerArounds)
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss()
+      }}
+    >
+      <View style={styles.container}>
+        <MapView
+          ref={_map}
+          showUserLoaction={true}
+          followUserLocation={true}
+          rotateEnabled={true}
+          zoomEnabled={true}
+          toolbarEnabled={true}
+          provider={PROVIDER_GOOGLE}
+          style={styles.bximg}
+        >
+          {centerArounds((item, index) => (
+            <MapView.Marker coordinate={item} key={index.toString()}>
+              <Image
+                source={require('../../assets/ambulance.png')}
+                resizeMode="cover"
+              />
+            </MapView.Marker>
+          ))}
+        </MapView>
+        {/* <Flex style={styles.bximg}>
+          <Image style={styles.img} source={require('../../assets/map.png')} />
+        </Flex> */}
+        <Entypo name="chevron-thin-left" style={styles.iconbtn} />
+        <View style={styles.from}>
+          <Flex
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Entypo name="location" size={24} color="black" />
+            <Flex style={{ marginLeft: 16 }}>
+              <Text style={{ fontWeight: 'bold', lineHeight: 24 }}>
+                Tên bệnh viện
+              </Text>
+              <Text style={{ fontSize: 14, color: 'rgba(0,0,0,0.6)' }}>
+                Địa chỉ chính xác
+              </Text>
+            </Flex>
+          </Flex>
+          <MaterialIcons name="favorite" size={20} color="black" />
+        </View>
+
+        <TouchableOpacity onPress={pressSetFrom}>
+          <View style={styles.btn}>
+            <Entypo name="direction" size={24} color="black" />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
+  )
+}
+
+export default SelectTo
 
 const styles = StyleSheet.create({
   container: {
@@ -70,58 +208,11 @@ const styles = StyleSheet.create({
 
     borderWidth: 1,
     borderColor: 'rgba(0,0,0,0.3)',
-    borderRadius: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+
     backgroundColor: 'rgba(0,0,0,0.1)',
   },
 })
-
-const SelectFrom = ({ navigation }) => {
-  const pressSetFrom = () => {
-    navigation.navigate('SetFrom')
-  }
-  const pressHanderBack = () => {
-    navigation.goBack()
-  }
-  return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss()
-      }}
-    >
-      <Flex style={styles.container}>
-        <Flex style={styles.bximg}>
-          <Image style={styles.img} source={require('../../assets/map.png')} />
-        </Flex>
-        <Entypo name="chevron-thin-left" style={styles.iconbtn} />
-        <Flex style={styles.from}>
-          <Flex
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Entypo name="location" size={24} color="black" />
-            <Flex style={{ marginLeft: 16 }}>
-              <Text style={{ fontWeight: 'bold', lineHeight: 24 }}>
-                Tên bệnh viện
-              </Text>
-              <Text style={{ fontSize: 14, color: 'rgba(0,0,0,0.6)' }}>
-                Địa chỉ chính xác
-              </Text>
-            </Flex>
-          </Flex>
-          <MaterialIcons name="favorite" size={20} color="black" />
-        </Flex>
-
-        <TouchableOpacity onPress={pressSetFrom}>
-          <View style={styles.btn}>
-            <Entypo name="direction" size={24} color="black" />
-          </View>
-        </TouchableOpacity>
-      </Flex>
-    </TouchableWithoutFeedback>
-  )
-}
-
-export default SelectFrom
